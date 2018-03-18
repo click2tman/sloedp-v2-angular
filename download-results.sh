a081b9dbaba7ffa814f175e2e5e156b85e5d21c5
Tamba Lamin <tamba@tambalamin.me>
2018-03-18 15:00:20 -0400

    latest results with split of mayor and chairperson

--- download-results.sh
+++ download-results.sh
@@ -12,8 +12,16 @@ pparliamentary=https://electiondata.io/api/results/parliamentary
 fcouncillor=/Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-councillor-election-results
 pcouncillor=https://electiondata.io/api/results/councillor
 
-fmayorchairperson=/Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-chair-election-results
-pmayorchairperson=https://electiondata.io/api/results/major-chair
+fchairperson=/Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-chairperson-election-results
+pchairperson=https://electiondata.io/api/results/chairperson
+
+fmayor=/Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-election-results
+pmayor=https://electiondata.io/api/results/major
+
+
+api/results/chairperson/all-chairperson-election-results.json
+api/results/major/all-major-election-results.json
+
 
 fvillageheadman=/Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-village-headman-election-results
 pvillageheadman=https://electiondata.io/api/results/
@@ -64,12 +72,20 @@ wget $pcouncillor/all-councillor-election-results.json
 cp all-councillor-election-results.json /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-councillor-election-results/
 
 
-#Download Mayor/Chair results
-rm -rf /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-mayor-chair-election-results/all-major-chair-election-results.json
-rm -rf /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-chair-election-results/all-major-chair-election-results.json
-cd /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-mayor-chair-election-results
-wget $pmayorchairperson/all-major-chair-election-results.json
-cp all-major-chair-election-results.json /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-chair-election-results/
+#Download Mayor results
+rm -rf /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-mayor-election-results/all-major-election-results.json
+rm -rf /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-election-results/all-major-election-results.json
+cd /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-mayor-election-results
+wget $pmayor/all-major-election-results.json
+cp all-major-election-results.json /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-mayor-election-results/
+
+#Download Chair results
+rm -rf /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-chairperson-election-results/all-chairperson-election-results.json
+rm -rf /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-chairperson-election-results/all-chairperson-election-results.json
+cd /Users/tamba.s.lamin/DEV/sloedp/www/assets/results/all-chairperson-election-results
+wget $pchairperson/all-chairperson-election-results.json
+cp all-chairperson-election-results.json /Users/tamba.s.lamin/DEV/sloedp/src/assets/results/all-chairperson-election-results/
+
 
 cd /Users/tamba.s.lamin/DEV/sloedp/
 git add --all
